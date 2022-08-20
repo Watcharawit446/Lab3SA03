00387fc4b1ba0afb4a621359a54667c8358fc624
6310110446 <110656157+Watcharawit446@users.noreply.github.com>
2022-08-20 15:51:11 +0700

    Update Hintstate.js

--- src/Hintstate.js
+++ src/Hintstate.js
@@ -1,26 +1,5 @@
 import React from "react";
 export default
 class Hint extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-          fhint: "Click here if you don't know."
-        }
-      }
-   
-      changeHint = () => {
-        this.setState({
-          fhint: "For Greeting"
-        })
-      }  
-      
-      render(){
-        return (
-          <div>
-            <p>{this.state.fhint}</p>
-            <button
-              type="button" onClick={this.changeHint} >Click !!</button>
-          </div>
-        );
-      }
+  
   }
