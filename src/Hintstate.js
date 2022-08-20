ddc3f08ecfd33278fdda3a1e799dc4f73f2887f3
6310110446 <110656157+Watcharawit446@users.noreply.github.com>
2022-08-20 16:09:39 +0700

    Edit Game Logic two

--- src/Hintstate.js
+++ src/Hintstate.js
@@ -1,5 +1,16 @@
 import React from "react";
 export default
 class Hint extends React.Component{
-  
+constructor(props){
+        super(props);
+        this.state = {
+          fhint: "Click here if you don't know."
+        }
+      }
+   
+      changeHint = () => {
+        this.setState({
+          fhint: "For Greeting"
+        })
+      }  
   }
